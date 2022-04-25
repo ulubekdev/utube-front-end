907eecb72f7a37a6073fd386a4ee8caba3ca06d4
Ulug'bek <93920465+theulubek@users.noreply.github.com>
2022-04-25 12:49:06 +0500

    shorten code

--- server.js
+++ server.js
@@ -7,20 +7,9 @@ const app = express();
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(process.cwd(), 'views', 'index.html'));
-});
-
-app.get('/register', (req, res) => {
-    res.sendFile(path.join(process.cwd(), 'views', 'register.html'));
-});
-
-app.get('/login', (req, res) => {
-    res.sendFile(path.join(process.cwd(), 'views', 'login.html'));
-});
-
-app.get('/admin', (req, res) => {
-    res.sendFile(path.join(process.cwd(), 'views', 'admin.html'));
-});
+app.get('/', (req, res) => res.sendFile(path.join(process.cwd(), 'views', 'index.html')));
+app.get('/register', (req, res) => res.sendFile(path.join(process.cwd(), 'views', 'register.html')));
+app.get('/login', (req, res) => res.sendFile(path.join(process.cwd(), 'views', 'login.html')));
+app.get('/admin', (req, res) => res.sendFile(path.join(process.cwd(), 'views', 'admin.html')));
 
 app.listen(PORT, () => console.log(`Client is running on port ${PORT}`));
